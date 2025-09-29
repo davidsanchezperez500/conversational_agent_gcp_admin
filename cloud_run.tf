1b28b8014b281f994c4e9f6e3a4f33620e6cbc01
David Sánchez <39903115+davidsanchezperez500@users.noreply.github.com>
2025-09-29 19:47:00 +0200

    feat: separate in new env security (#6)
    * feat: separate in new env security
    
    * feat: separate environments

--- cloud_run.tf
+++ cloud_run.tf
@@ -5,10 +5,10 @@ module "frontend_service" {
   region       = var.region
   environment  = var.environment
   service_name = "frontend-agent"
-  image_url    = "us-east1-docker.pkg.dev/conversational-agent-${var.environment}/docker-repository-${var.environment}/frontend:latest"
+  image_url    = "us-east1-docker.pkg.dev/conversational-agent-commonsec/docker-repository-${var.environment}/frontend:latest"
 
   secrets = {
-    "API_KEY" = google_secret_manager_secret.api_key_front.id
+    "API_KEY" = "projects/579307523881/secrets/api-key-front-${var.environment}"
   }
 
   ingress_traffic = "INGRESS_TRAFFIC_ALL"
@@ -25,11 +25,6 @@ resource "google_cloud_run_v2_service_iam_member" "frontend_invokes_chatbot" {
   member   = "serviceAccount:${module.frontend_service.service_account_email}"
 }
 
-resource "google_secret_manager_secret_iam_member" "secret_binding" {
-  secret_id = google_secret_manager_secret.api_key_front.id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${module.frontend_service.service_account_email}"
-}
 
 # Clud Run service for the chatbot agent backend
 module "chatbot_agent" {
@@ -38,7 +33,7 @@ module "chatbot_agent" {
   region           = var.region
   environment      = var.environment
   service_name     = "chatbot-agent"
-  image_url        = "us-east1-docker.pkg.dev/conversational-agent-${var.environment}/docker-repository-${var.environment}/chatbot-backend:latest"
+  image_url        = "us-east1-docker.pkg.dev/conversational-agent-commonsec/docker-repository-${var.environment}/chatbot-backend:latest"
   vpc_connector_id = google_vpc_access_connector.vpc_connector.id
   business_tags    = local.business_tags
   component_label  = "backend"
@@ -60,7 +55,7 @@ module "conversational_agent" {
   region           = var.region
   environment      = var.environment
   service_name     = "conversational-agent"
-  image_url        = "us-east1-docker.pkg.dev/conversational-agent-${var.environment}/docker-repository-${var.environment}/conversational-backend:latest"
+  image_url        = "us-east1-docker.pkg.dev/conversational-agent-commonsec/docker-repository-${var.environment}/conversational-backend:latest"
   vpc_connector_id = google_vpc_access_connector.vpc_connector.id
   business_tags    = local.business_tags
   component_label  = "backend"
@@ -73,40 +68,4 @@ module "conversational_agent" {
   role     = "roles/bigtable.user"
   member   = "serviceAccount:${module.conversational_agent.service_account_email}" 
 } */
-
-resource "google_pubsub_topic_iam_member" "conversational_publisher_access" {
-  project = var.project_id
-  topic   = google_pubsub_topic.vertex_request_topic.name
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${module.conversational_agent.service_account_email}"
-}
-
-# Cloud Run service for the worker backend  
-module "worker_agent" {
-  source           = "./modules/cloud-run-service"
-  project_id       = var.project_id
-  region           = var.region
-  environment      = var.environment
-  service_name     = "worker-agent"
-  image_url        = "us-east1-docker.pkg.dev/conversational-agent-${var.environment}/docker-repository-${var.environment}/worker-backend:latest"
-  vpc_connector_id = google_vpc_access_connector.vpc_connector.id
-  cpu_limit        = var.cpu_limit_cloud_run_worker
-  memory_limit     = var.memory_limit_cloud_run_worker
-  business_tags    = local.business_tags
-  component_label  = "backend"
-  depends_on       = [google_project_iam_policy.conversational_agent_project]
-}
-
-/* resource "google_bigtable_instance_iam_member" "worker_bigtable_access" {
-  project  = var.project_id
-  instance = google_bigtable_instance.conversational_agent.name
-  role     = "roles/bigtable.user"
-  member   = "serviceAccount:${module.worker_agent.service_account_email}" 
-}
- */
-resource "google_project_iam_member" "vertex_ai_user_binding" {
-  project = var.project_id
-  role    = "roles/aiplatform.user"
-  member  = "serviceAccount:${module.worker_agent.service_account_email}"
-}
 
